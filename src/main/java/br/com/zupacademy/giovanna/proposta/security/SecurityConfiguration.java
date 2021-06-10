d26d678b32b9624014dea46e07a1768b807c9198
giovanna <giovanna.bernardi@zup.com.br>
2021-06-10 11:49:43 -0300

    Login via senha cartão

---
+++ src/main/java/br/com/zupacademy/giovanna/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.giovanna.proposta.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().httpStrictTransportSecurity().disable();
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/**").hasAuthority("SCOPE_propostas")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+}
